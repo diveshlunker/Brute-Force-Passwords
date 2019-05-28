1b9fffd9a4f01fb8386e2e5d3e77447605b7a04e
Divesh <diveshdj92@gmail.com>
2019-05-28 18:58:40 +0530

    n number of passwords generated

--- js/index.js
+++ js/index.js
@@ -2,11 +2,20 @@ var keylist = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_ABCDEFGHIJKLMNOPQRST
 var tmp = ''
 
 function generatepass(plength,pstart,pnumber,pstartlength){
-    tmp=pstart
-    for(i=0;i<plength-pstartlength;i++){
+    var l =[]
+    for(j=0;j<pnumber;j++){
+        tmp=pstart
+        for(i=0;i<plength-pstartlength;i++){
         tmp+=keylist.charAt(Math.floor(Math.random()*keylist.length))
+        }
+        l.push(tmp)
+    }
+    tmp=""
+    for(i=0;i<pnumber;i++){
+        tmp+=l[i]+'\n'
     }
     return tmp
+    
 }
 
 
