695a989443670dfbbe2c4a45bb6ec526a6ead591
fn <46826941+ForgivenNin@users.noreply.github.com>
2020-02-04 18:28:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-function populateform(plength, pstart){
+async function populateform(plength, pstart){
+    document.getElementById('stop').value = 'Stop'
     var keylist = "abcdefghijklmnopqrstuvwxyz1234567890!-@#$%^&*_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var tmp = ''
     var l = []
     // Check for "/" at end of string
     if (pstart.charAt(pstart.length-1) !== "/") {
       pstart += "/"
     }
-    while (document.getElementById('stop').value === false){
-        tmp = pstart
+    while (document.getElementById('stop').value === 'Stop'){
+        var tmp = ''
         for(var i = 0; i < plength; i++){
           tmp += keylist.charAt(Math.floor(Math.random() * keylist.length))
-          var failed = []
-          fetch(tmp, {
-            method: 'POST',
-            mode: 'no-cors',
-            credentials: 'include',
-            referrerPolicy: 'no-referrer'
-          }).then((response) => {
-            if (!response.ok) {
-              failed[i] = tmp
-            }
-          })
-          .catch((error) => {
-            failed[i] = tmp
-          })
         }
+        var failed = []
+        fetch((pstart + tmp), {
+          method: 'POST',
+          mode: 'no-cors',
+          credentials: 'include',
+          referrerPolicy: 'no-referrer'
+        }).then((response) => {
+          if (!response.ok) {
+            failed[i] = tmp
+            console.log("Couldn't fetch.");
+          }
+        })
+        .catch((error) => {
+          failed[i] = tmp
+        })
         // Check for connections, clones, and fails
         if ((failed.includes(tmp))||(l.includes(tmp))){
-            j-=1
+            i -= 1
         } else {
             l.push(tmp);
         }
         if(l.length > 0){
             var tmp2 = ""
-            for(i = 0; i < l.length; i++){
-                tmp2 += l[i] + '\n'
-            }   
+            for(var j = 0; j < l.length; j++){
+                tmp2 += pstart + l[j] + '\n'
+            }
         } else {
             tmp2 = l[0];
         }
         document.passgen.output.value = tmp2
         document.getElementById("fileButton").disabled = false
-        $("#fileButton").click(function(){
-        download("link-force.txt", tmp2)
-        })
     }
 }
 
